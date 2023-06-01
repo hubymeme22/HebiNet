5f44617ef08ed7aed0d56bbd539390d2bb112ac7
HueHueberry <huby.new1112@gmail.com>
2023-06-02 00:23:23 +0800

    initialized accept condition template and samples

--- libraries/HebiServer.h
+++ libraries/HebiServer.h
@@ -1,22 +1,38 @@
 #include "TCPService.h"
+#include "CollEnc.h"
+
+#include <cstring>
+#include <string>
+#include <map>
 
 class HebiNetServer: public Server {
     private:
         const char* key;
+        int keySize;
+
     public:
         HebiNetServer(const char*, int, const char*);
-        void recieveProcess(char*, int);
-        void loopedListen();
+
+        virtual bool acceptCondition(char*, int);
+        virtual void recieveProcess(int, char*, int);
 };
 
+// HebiNetServer constructor
 HebiNetServer::HebiNetServer(const char* ip, int port, const char* key): Server(ip, port) {
     this->key = key;
+    this->keySize = strlen(key);
 }
 
-void HebiNetServer::recieveProcess(char* buffer, int bufferSize) {
-    printf("Recieved override: %s", buffer);
+// initial condition from packet to indicate if the client will be accepted
+bool HebiNetServer::acceptCondition(char* buffer, int bufferSize) {
+    return (strncmp(buffer, "hebi_child_server", 17) == 0);
 }
 
-void HebiNetServer::loopedListen() {
-    Server::loopedListen();
+// overriden recieve process from server
+void HebiNetServer::recieveProcess(int id, char* buffer, int bufferSize) {
+    if (this->acceptCondition(buffer, bufferSize))
+        printf("Thread[%d] Recieved override: %s", id, buffer);
+
+    // close the connection for the client if not accepted
+    else this->closeConnection(id);
 }
