8170e809fce8d23b72d0f0b499ed6fb13c08e78f
HueHueberry <huby.new1112@gmail.com>
2023-06-18 22:52:25 +0800

    demonstrate sending of encrypted packets

--- SampleHebiClient.cpp
+++ SampleHebiClient.cpp
@@ -1,7 +1,22 @@
 #include "libraries/HebiClient.h"
+#include <iostream>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::getline;
 
 int main() {
     HebiNetClient client = HebiNetClient("0.0.0.0", 8080, "rickandmorty");
     client.connectByMain();
-    client.loopedListen();
+    client.threadedListen();
+
+    while (true) {
+        string message;
+        cout << "Enter a message: ";
+        getline(cin, message);
+
+        const char* msg = message.c_str();
+        client.sendMsg((char*)msg, strlen(msg));
+    }
 }
